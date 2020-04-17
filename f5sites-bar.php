97155121c84186cdd0a3c58b764e34c0cb9b40e1
Francisco Mat <francisco@f5sites.com>
2020-04-16 23:58:22 -0300

    brand logo link

--- f5sites-bar.php
+++ f5sites-bar.php
@@ -25,7 +25,7 @@ function generate_f5sites_bar() { ?>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	
 	<nav class="navbar navbar-expand-lg navbar-light bg-light navbar-f5sites-bar">
-		<a href="https://www.f5sites.com/startups-navigator/" class="alogo navbar-brand" data-toggle='popover' data-placement='bottom' title="F5 Sites Startups Navigator" data-trigger='hover' data-content="Portuguese: Navegador de Startups F5 Sites"><img src='<?php echo plugins_url( "f5sites.com-logo.png", __FILE__ ); ?>' alt="F5 Sites" /></a>
+		<a href="https://www.f5sites.com/" class="alogo navbar-brand" data-toggle='popover' data-placement='bottom' title="F5 Sites Startups Navigator" data-trigger='hover' data-content="Portuguese: Navegador de Startups F5 Sites"><img src='<?php echo plugins_url( "f5sites.com-logo.png", __FILE__ ); ?>' alt="F5 Sites" /></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarF5links" aria-controls="navbarF5links" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
