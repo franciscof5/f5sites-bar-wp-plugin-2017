27e46dd3c3a337af3cc2200ee339d9152ad88454
Francisco Mat <francisco@f5sites.com>
2020-04-17 00:54:28 -0300

    bug not fixed

--- f5sites-bar.php
+++ f5sites-bar.php
@@ -19,7 +19,7 @@ add_action('wp_enqueue_scripts', 'load_scritps_f5sites_bar');
 function generate_f5sites_bar() { ?>
 	<?php 
 	/*-*/ ?>
-	<?php if(strpos($_SERVER["REQUEST_URI"], "franciscomat")) { ?>
+	<?php if(!strpos($_SERVER["HTTP_HOST"], "franciscomat")) { ?>
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<?php } ?>
 	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
